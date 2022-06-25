aab035f9e3dc622eff3df352a582eb86cf65e1f6
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2022-06-25 16:30:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const fullName="Abdulrasheed Abdulsalam"
+const country="Nigeria"
+const height= "160cm"
+document.write(`My name is ${fullName},<br> I'm from ${country}, <br> I am ${height} tall. `)
